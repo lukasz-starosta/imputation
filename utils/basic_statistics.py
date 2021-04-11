d0f853eeb53e6d1a52f18bc7edd772583aae6c5c
Łukasz Starosta <lstarosta98@gmail.com>
2021-04-11 15:33:58 +0200

    Updated statistics

--- utils/basic_statistics.py
+++ utils/basic_statistics.py
@@ -8,35 +8,47 @@ condition = lambda column : (column != 'Model' and column != 'Release date')
 def basic_statistics(df):
     new_df = []
     columns = []
+
+    mean = np.nan
+    std = np.nan
+    mode = np.nan
+    md = np.nan
+    qt1 = np.nan
+    qt3 = np.nan
+
     for column in df:
         if condition(column):
-            #-----------srednia------------
-            mean = df[column].mean()
-            mean = round(mean,2)
-            #----odchylenie standardowe----
-            std = df[column].std()
-            std = round(std,2)
-            #-----------moda---------------
-            mode = df[column].mode()
             try:
+                #-----------srednia------------
+                mean = df[column].mean()
+                mean = round(mean,2)
+
+                #----odchylenie standardowe----
+                std = df[column].std()
+                std = round(std,2)
+
+                #-----------moda---------------
+                mode = df[column].mode()
                 mode = round(mode[0])
+
+                #-----------mediana------------
+                md = df[column].median()
+                md = round(md,2)
+
+                #-----------kwartyl 1------------
+                qt1 = np.percentile(df[column], 25)
+                qt1 = round(qt1,2)
+
+                #-----------kwartyl 3------------
+                qt3 = np.percentile(df[column], 75)
+                qt3 = round(qt3,2)
+
             except KeyError:
                 print('This data frame lacks too many data!')
 
-            #-----------mediana------------
-            md = df[column].median()
-            md = round(md,2)
-            #-----------kwartyl------------
-            qt = df[column].quantile()
-            qt = round(qt,2)
-
-
-            new_df.append([mean,std, mode, mode, qt])
+            new_df.append([mean,std, mode, md, qt1, qt3])
             columns.append(column)
 
-
-
     new_df = pd.DataFrame(new_df, index=columns)
-    print('\n',tabulate(new_df, headers = ['mean','std','mode','md','qt']),'\n')
+    print('\n',tabulate(new_df, headers = ['mean','std','mode','median','1st quartile', '3rd quartile']),'\n')
     return new_df
-        
