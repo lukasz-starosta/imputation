1605b68ca5d06e71682fc42e472ffc64e8418277
KrzysztofMoszczynski <luxtorpeda0104@gmail.com>
2021-04-11 20:18:07 +0200

    Małe zmiany

--- methods/mice.py
+++ methods/mice.py
@@ -1,13 +1,13 @@
-#from fancyimpute import KNN, NuclearNormMinimization, SoftImpute, BiScaler, IterativeImputer
+from fancyimpute import KNN, NuclearNormMinimization, SoftImpute, BiScaler, IterativeImputer
 import numpy as np
 import pandas as pd
 from tabulate import tabulate
 
 
 def mice(filename):
-    '''df = pd.read_csv(filename)
+    df = pd.read_csv(filename)
     df = df.drop('Model',1)
     MICE_imputer = IterativeImputer()
     df_MICE = df.copy(deep= True)
     df_MICE.iloc[:, :] = MICE_imputer.fit_transform(df_MICE)
-    return df_MICE'''
+    return df_MICE
